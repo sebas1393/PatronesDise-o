3e2ff92f35f9693821768c8a297562dd187706a6
jsebas1393@gmail.com <sebitasbueno1@gmail.com>
2021-08-22 21:50:59 -0500

    Singleton Pattern

---
+++ Patrones de diseño/Patrones de diseño/Program.cs
@@ -0,0 +1,205 @@
+﻿using Patrones_de_diseño.Singleton;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace Patrones_de_diseño
+{
+    class Program
+    {
+
+        static string[] patrones = new string[] { "SINGLETON", "PROTOTYPE", "FACTORY" };
+        static async Task Main(string[] args)
+        {
+            bool flag = true;
+            Console.WriteLine("*************************************************************************************************");
+            Console.WriteLine("BIENVENIDO AL PROGRAMA DE PATRONES DE SEBAS!!");
+
+            //Menu
+            do
+            {
+                
+                // Pinta las opciones
+                int cont = 1;
+                Console.WriteLine("Elija la opción deseada:");
+                foreach (string patron in patrones)
+                {
+                    Console.WriteLine($"    {cont}- {patron}.");
+                    cont++;
+                }
+
+                Console.WriteLine("    9- Salir del sistema.") ;
+
+                //Captura la opción del usuario
+                int opcion = 0;
+                _ = int.TryParse(Console.ReadLine(), out opcion);
+
+                //Valida la opción
+                if (opcion == 9) {
+                    
+                    flag = false;
+                    continue;
+                }
+                if (opcion == 0 || opcion > patrones.Length)
+                {
+                    Console.WriteLine("Elija una opción valida");
+                    continue;
+                }
+                
+                switch (opcion)
+                {
+                    ///Patron Singleton
+                    case 1:
+                        await menuSingleton();
+                        break;
+                    //Patron Prototype
+                    case 2:
+                        ImplementsPrototype();
+                        break;
+                    default:
+                        Console.WriteLine("Patron no implementado");
+                        break;
+                }
+
+            } while (flag);
+            Console.WriteLine("Hasta luego.");
+            Console.ReadKey();
+        }
+
+        #region Singleton implementations
+        private static async Task menuSingleton()
+        {
+            int singletonType = 0;
+
+            //Menu singleton
+            while (singletonType == 0)
+            {
+
+                Console.WriteLine("*******************Elija una implementación del singleton*********************:");
+                //Standart para todos los singletons (Sinc Aplications).
+                Console.WriteLine(" 1- Singleton con constructor estatico.");
+                
+                Console.WriteLine(" 2- Problemas del singleton, perdida de control de instanciación.");
+                
+                Console.WriteLine(" 3- Singleton con inicialización estática.");
+                
+                //Variante para Multithreading
+                Console.WriteLine(" Singleton para multithreading:");
+                Console.WriteLine(" Consideraciones: El Lock siempre genera un alto procesamiento, evitar el uso excesivo, es mejor el doublecheck en los singletons");
+                Console.WriteLine("     4- Doble validación y hacer lock del objeto de la instancia para evitar generación de varias instancias \n" +
+                                  "        En un ambiente multihilos");
+                Console.WriteLine("     5- Lock antes de validar el objeto, no se recomienta esta práctica ya que el uso de la \n" +
+                                  "        memoria es más costoso");
+                //Lee la opción del usuario
+                _ = int.TryParse(Console.ReadLine(), out singletonType);
+
+                //Valida opción
+                if (singletonType == 0 || singletonType > 10)
+                {
+                    Console.WriteLine("elija una opción valida");
+                    singletonType = 0;
+                    continue;
+                }
+                //Implementa el singleton segun la opción
+                switch (singletonType)
+                {
+                    //Constructor estatico.
+                    case 1:
+                        ImplementsSingleton();
+                        break;
+                    //Perdida de manejo en la construcción del objeto
+                    case 2:
+                        ImplementsSingletonIssue();
+                        break;
+                    //Singleton del objeto en la variable estatica
+                    case 3:
+                        SingletonStaticInitialization();
+                        break;
+                    //Doble checkeo de la variable y lock dentro de la primer validación
+                    case 4:
+                        for (int i = 0; i < 3; i++)
+                            await SingletonDoubleCheck();
+                        break;
+                    //Lock Directo, no usar para aplicaciones con mucho tráfico, usar opción numero 4.
+                    case 5:
+                        List<DirectLockSingleton> instances = new List<DirectLockSingleton>();
+                        for (int i = 0; i < 3; i++)
+                            instances.Add(await SingleLockSingleton());
+                        Console.WriteLine(instances[0].Name == instances[1].Name && 
+                            instances[1].Name == instances[2].Name ? "Se crearon varios objetos de la misma instancia.": "Hay Varias instacias.") ;                        
+
+                        
+                        break;
+                }
+
+            }
+        }
+
+        private static void ImplementsSingletonIssue()
+        {
+            Console.WriteLine($"El valor de mi entero es: {Singleton.Singleton.MyInt}");
+        }
+
+        private static void SingletonStaticInitialization()
+        {
+            Singleton.StaticInitializationSingleton inicializacionEstatica = Singleton.StaticInitializationSingleton.GetInstance;
+
+            Singleton.StaticInitializationSingleton instanciados = Singleton.StaticInitializationSingleton.GetInstance;
+
+            if (inicializacionEstatica.Equals(instanciados))
+            {
+                Console.WriteLine("la instancia uno y dos son la misma instancia ");
+            }
+        }
+
+        private static async Task SingletonDoubleCheck() {
+            // Usa la instanciación con doble validación
+            DoubleCheckSingleton instance = DoubleCheckSingleton.GetInstance;
+            Console.WriteLine($"Cantidad de instancias iniciadas {DoubleCheckSingleton.totalInstances}");
+
+
+        }
+        private static void ImplementsSingleton()
+        {
+            Console.WriteLine("***Singleton Pattern demonstration************");
+
+            //No se puede hacer una instanciación new debido a que el constructor es privado.
+            //Singleton.Carrito carrito = new Singleton.Carrito(); //Esto fallaria.
+
+            //Tengo que acceder a la instancia, esta propiedad es unica y no puede ser modificada desde afuera, el control lo tiene la clase misma.
+            Singleton.Singleton carritoPrimeraInstancia = Singleton.Singleton.GetInstance;
+
+            //Segunda instancia, al acceder a la misma propiedad estatica, me asegura que accedo a la misma instancia que la primera.
+            Singleton.Singleton carritoSegundaInstancia = Singleton.Singleton.GetInstance;
+
+            //Comprobación de que los objetos apuntan a la misma dirección de memoria.
+            if (carritoPrimeraInstancia.Equals(carritoSegundaInstancia))
+            {
+                Console.WriteLine("La primera y segunda instancia son la misma.");
+
+            }
+            else
+            {
+                Console.WriteLine("Existen diferente instancias");
+            }
+        }
+
+        private static async Task<DirectLockSingleton> SingleLockSingleton() {
+            DirectLockSingleton instance = DirectLockSingleton.GetInstance;
+            Random ram = new Random();
+            int number = ram.Next(0, 5);
+            instance.Name = $"instance {number}"; 
+            return instance;
+        }
+        #endregion
+        private static void ImplementsPrototype()
+        {
+
+            //Por implementar.
+            Console.WriteLine("Por implementar");
+
+        }
+
+
+    }
+}
---
+++ Patrones de diseño/Patrones de diseño/Singleton/Singleton.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Clases que implementan el patron singleton.
+/// </summary>
+namespace Patrones_de_diseño.Singleton
+{
+    /// <summary>
+    /// Implementación standart del patron singleton usando un constructor estatico para instanciación de un solo objeto de clase.
+    /// Esta implementación asegura el uso de una sola instancia de la clase en todo el sistema, si se crean referencias desde variables, apuntaran
+    /// a la misma dirección de memoria.
+    /// </summary>
+    public sealed class Singleton
+    {
+        #region Singleton implementation using static constructor
+        private static readonly Singleton Instance;
+        private static int TotalInstances;
+        /*
+         * Private constructor is used to prevent
+         * creation of instances with the 'new' keyword
+         * outside this class.
+         */
+        private Singleton()
+        {
+            Console.WriteLine("--Private constructor is called.");
+            Console.WriteLine("--Exit now from private constructor.");
+        }
+        /*
+         * A static constructor is used for the following purposes:
+         * 1. To initialize any static data
+         * 2. To perform a specific action only once
+         *
+         * The static constructor will be called automatically before:
+         * i. You create the first instance; or
+         * ii.You refer to any static members in your code.
+         *
+         */
+        // Here is the static constructor
+        static Singleton()
+        {
+            // Printing some messages before you create the instance
+            Console.WriteLine("Static constructor is called");
+            Instance = new Singleton();
+            TotalInstances++;
+            Console.WriteLine($"- Singleton instance is created. Number of instances: {TotalInstances}");
+            Console.WriteLine("- Exit from static constructor.");
+        }
+        /*
+      * If you like to use expression-bodied read-only
+      * property, you can use the following line (C# v6.0 onwards).
+      */
+        // public static Singleton GetInstance => Instance;
+        #endregion
+        public static Singleton GetInstance => Instance;
+        /* The following line is used to discuss
+        the drawback of the approach. */
+
+        public static int MyInt = 25;
+    }
+    /// <summary>
+    /// No declara el constructor estatico, usa directamente la variable para instanciar el objeto.
+    /// Buena practica si no se requiere que el constructor haga alguna acción.
+    /// </summary>
+    public sealed class StaticInitializationSingleton
+    {
+
+        private static readonly StaticInitializationSingleton Instance = new StaticInitializationSingleton();
+        private StaticInitializationSingleton()
+        {
+
+        }
+        public static StaticInitializationSingleton GetInstance => Instance;
+
+    }
+    /// <summary>
+    /// Checkea dos veces si el objeto esta siendo instanciado multiples veces, asegura que solo un hilo pueda instanciarlo y los demas no.
+    /// </summary>
+    public sealed class DoubleCheckSingleton
+    {
+        //Se usa volatile para asegurar que la asignación a la variable instancia
+        //Halla Finalizado antes de ser accesada.
+        private static volatile DoubleCheckSingleton Instance;
+        public static object lockObject = new object();
+        public static int totalInstances;
+        private DoubleCheckSingleton() {
+            Console.WriteLine("Se inicio una nueva instancia de el objeto");
+            totalInstances++;
+        }
+        /// <summary>
+        /// Variable estatica instanciadora
+        /// </summary>
+        public static DoubleCheckSingleton GetInstance
+        {
+            get
+            {
+                //Si la instancia es nula
+                if (Instance == null)
+                {
+                    //Bloquea el objeto cuando la instancia esta nula.
+                    lock (lockObject)
+                    {
+                        // Doble validación para evitar la multiple instanciación
+                        if (Instance == null)
+                        {
+                            Instance = new DoubleCheckSingleton();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+
+
+    }
+    /// <summary>
+    /// Realiza Lock de todos los hilos que ingresan al metodo, si la instancia es null genera una nueva instancia, de lo contrario genera una nueva. 
+    /// Muy costoso, prevenir su uso si es una aplicación de uso costoso.
+    /// </summary>
+    public sealed class DirectLockSingleton {
+        private static volatile DirectLockSingleton Instance;
+        private static object lockObject = new object();
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private DirectLockSingleton() { }
+
+        public static DirectLockSingleton GetInstance {
+            get {
+                lock (lockObject) {
+                    if (Instance == null) {
+                        Instance = new DirectLockSingleton();
+                    }
+                }
+                return Instance;
+            }
+        }
+            
+    }
+}
